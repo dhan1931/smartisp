5ec59db6e1fff681d90d5e992977bedda64d4e77
Ale <acercado28@ggmail.com>
2026-09-05 23:37:11 -0500

    Incluir pagina de recuperacion en Vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        admin: 'admin.html'
+        admin: 'admin.html',
+        resetPassword: 'reset-password.html'
       }
     }
   }
