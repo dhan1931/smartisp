9520ab0f82a84e52a25ab76cfd3991a5583cff57
Ale <acercado28@ggmail.com>
2026-09-14 01:29:00 -0500

    feat: permitir personalizar el diseno del logo y subir imagen en PNG desde el panel de control

--- server.js
+++ server.js
@@ -124,7 +124,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(session({
   name: 'nexotech.sid',
   secret: process.env.SESSION_SECRET || 'cambia-esta-clave-en-produccion',
